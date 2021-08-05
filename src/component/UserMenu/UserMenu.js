e313d7dcc1d1d3f761a0792f9bba19a85a8e5790
KristinaY-87 <72267606+KristinaY-87@users.noreply.github.com>
2021-08-05 19:01:10 +0300

    add settings

--- src/component/UserMenu/UserMenu.js
+++ src/component/UserMenu/UserMenu.js
@@ -4,20 +4,6 @@ import { authSelectors, authOperations } from '../../redux/auth';
 import defaultAvatar from './default-avatar.png';
 import s from './UserMenu.module.css';
 
-// const styles = {
-//   container: {
-//     display: 'flex',
-//     alignItems: 'center',
-//   },
-//   avatar: {
-//     marginRight: 4,
-//   },
-//   name: {
-//     fontWeight: 700,
-//     marginRight: 12,
-//   },
-// };
-
 const UserMenu = ({ avatar, name, onLogout }) => (
   <div className={s.container}>
     <img src={avatar} alt="" width="32" className={s.avatar} />
